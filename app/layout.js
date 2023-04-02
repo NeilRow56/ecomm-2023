ccf0a046015a70207663eec1915a9077dbd41d0f
Neil Rowland <neilrowland56@gmail.com>
2023-04-02 13:56:17 +0100

    Added ClientOnly comonent

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,27 @@
-import Navbar from './components/navbar/Navbar';
-import './globals.css'
+import ClientOnly from "./components/ClientOnly";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
 
-import { Nunito } from 'next/font/google'
+import { Nunito } from "next/font/google";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb app create next app',
-}
+  title: "Airbnb",
+  description: "Airbnb app create next app",
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+  subsets: ["latin"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      className={font.className}
-      >
-        <Navbar />
+      <body className={font.className}>
+        <ClientOnly>
+          <Navbar c />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
